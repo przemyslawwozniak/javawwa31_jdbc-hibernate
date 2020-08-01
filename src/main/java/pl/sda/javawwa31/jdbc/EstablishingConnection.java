1b8864c7f6c8b7bcca61731c4c05d7e6223f01ee
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-08-01 12:25:44 +0200

    Demoed database read with Statement

--- src/main/java/pl/sda/javawwa31/jdbc/EstablishingConnection.java
+++ src/main/java/pl/sda/javawwa31/jdbc/EstablishingConnection.java
@@ -2,9 +2,7 @@ package pl.sda.javawwa31.jdbc;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.TimeZone;
 
@@ -23,14 +21,25 @@ public class EstablishingConnection {
             System.err.println("Nie mozna otworzyc pliku konfiguracyjnego: " + ioe);
         }
 
+        final String query = "select customerName, country from customers";
         //od tej linii obiekt klasy Properties zawiera odczytana konfiguracje
         try(Connection connection = DriverManager.getConnection(
                 //configuration.get("db.url").toString() + "?serverTimezone=" + TimeZone.getDefault().getID(),
                 configuration.get("db.url").toString(),
                 configuration.get("db.user").toString(),
-                configuration.get("db.pswd").toString()
-        )) {
+                configuration.get("db.pswd").toString());
+
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+        ) {
             System.out.println("Nawiazano polaczenie z baza danych.");
+
+            System.out.println("Oto nasi klienci z podzialem na kraje");
+            System.out.println("Client name | Country");
+
+            while(result.next()) {
+                System.out.println(result.getString("customerName") + " | " + result.getString("country"));
+            }
         }
         catch(SQLException sex) {
             System.err.println("Blad nawiazywania polaczenia z baza danych: " + sex);
